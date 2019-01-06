7f575f7e15b7a161a4f6c3ea71d08c8af268682c
himandroid <him20488@gmail.com>
2019-01-06 10:29:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,9 @@
 var express = require('express'),
-   // bodyParser = require('body-parser'),
-   // request = require('request'),
-  //  qrcode = require('qrcode-npm'),
-//    decode = require('salesforce-signed-request'),
-
-    consumerSecret = process.env.CONSUMER_SECRET,
-
-    app = express();
-
-//app.set('view engine', 'ejs');
-//app.use(bodyParser()); // pull information from html in POST
-//app.use(express.static(__dirname + '/public'));
-
+consumerSecret = process.env.CONSUMER_SECRET,
+app = express();
 app.post('/signedrequest', function(req, res) {
-
-    // You could save this information in the user session if needed
-    /* var signedRequest = decode(req.body.signed_request, consumerSecret),
-        context = signedRequest.context,
-        oauthToken = signedRequest.client.oauthToken,
-        instanceUrl = signedRequest.client.instanceUrl,
-        query = "SELECT Id, FirstName, LastName, Phone, Email FROM Contact WHERE Id = '" + context.environment.record.Id + "'",
-        contactRequest = {
-            url: instanceUrl + '/services/data/v29.0/query?q=' + query,
-            headers: {
-                'Authorization': 'OAuth ' + oauthToken
-            }
-        };
-    request(contactRequest, function(err, response, body) {
-        var qr = qrcode.qrcode(4, 'L'),
-            contact = JSON.parse(body).records[0],
-            text = 'MECARD:N:' + contact.LastName + ',' + contact.FirstName + ';TEL:' + contact.Phone + ';EMAIL:' + contact.Email + ';;';
-        qr.addData(text);
-        qr.make();
-        var imgTag = qr.createImgTag(4);
-        res.render('index', {context: context, imgTag: imgTag});
-    }); */
-
-	var jsonres = {
+//sending static response
+  var jsonres = {
   "region": "US",
   "serialNumber": "1234,456"
 };
@@ -44,7 +11,6 @@ app.post('/signedrequest', function(req, res) {
 });
 
 app.set('port', process.env.PORT || 5000);
-
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
